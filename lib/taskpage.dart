39b02d45ed1ae0d7d77665ef2986fa8602827157
Ashish Mohapatra <78657461+ashishmohapatra240@users.noreply.github.com>
2021-12-20 20:07:29 +0530

    Add files via upload

---
+++ lib/taskpage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TaskPage extends StatefulWidget {
+  const TaskPage({ Key? key }) : super(key: key);
+
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+
+  }
+}
